aeb2e89045b47fab3adc723dfc678328dbb8b927
Vivien Kraus <vivien.kraus@univ-reims.fr>
2022-06-29 14:10:31 +0200

    Prepare source for emscripten.

--- include/adftool.h
+++ include/adftool.h
@@ -10,73 +10,99 @@ typedef void *hid_t;
 #include <hdf5.h>
 #endif /* not LIBADFTOOL_WITHOUT_HDF5 */
 
+#if BUILDING_LIBADFTOOL && HAVE_EMSCRIPTEN_H
+#include "emscripten.h"
+#define LIBADFTOOL_DLL_EXPORTED EMSCRIPTEN_KEEPALIVE
+#elif BUILDING_LIBADFTOOL && HAVE_VISIBILITY
+#define LIBADFTOOL_DLL_EXPORTED __attribute__((__visibility__("default")))
+#elif BUILDING_LIBADFTOOL && defined _MSC_VER
+#define LIBADFTOOL_DLL_EXPORTED __declspec(dllexport)
+#elif defined _MSC_VER
+#define LIBADFTOOL_DLL_EXPORTED __declspec(dllimport)
+#else
+#define LIBADFTOOL_DLL_EXPORTED
+#endif
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif				/* __cplusplus */
 
-/* Keys are compared against each other in a B+ tree. However, since
-   we are defining an index, some keys are already known and so we
-   just use their index in the key table, or they are not known. For
-   instance, the key used in a query can be random and thus we know
-   its literal value, but it does not exist in the table. */
+  /* Keys are compared against each other in a B+ tree. However, since
+     we are defining an index, some keys are already known and so we
+     just use their index in the key table, or they are not known. For
+     instance, the key used in a query can be random and thus we know
+     its literal value, but it does not exist in the table. */
 
-  int adftool_with_hdf5 (void);
+  extern LIBADFTOOL_DLL_EXPORTED int adftool_with_hdf5 (void);
 
   struct adftool_bplus_key;
 
-  struct adftool_bplus_key *adftool_bplus_key_alloc (void);
+  extern LIBADFTOOL_DLL_EXPORTED
+    struct adftool_bplus_key *adftool_bplus_key_alloc (void);
 
-  void adftool_bplus_key_free (struct adftool_bplus_key *key);
+  extern LIBADFTOOL_DLL_EXPORTED
+    void adftool_bplus_key_free (struct adftool_bplus_key *key);
 
   /* Return 0 if no error, something else if the key is not of the
      correct type. */
-  int adftool_bplus_key_get_known (const struct adftool_bplus_key *key,
-				   uint32_t * index);
+  extern LIBADFTOOL_DLL_EXPORTED
+    int adftool_bplus_key_get_known (const struct adftool_bplus_key *key,
+				     uint32_t * index);
 
-  int adftool_bplus_key_get_unknown (const struct adftool_bplus_key *key,
-				     void **key_value);
+  extern LIBADFTOOL_DLL_EXPORTED
+    int adftool_bplus_key_get_unknown (const struct adftool_bplus_key *key,
+				       void **key_value);
 
-  void adftool_bplus_key_set_known (struct adftool_bplus_key *key,
-				    uint32_t key_index);
+  extern LIBADFTOOL_DLL_EXPORTED
+    void adftool_bplus_key_set_known (struct adftool_bplus_key *key,
+				      uint32_t key_index);
 
-  void adftool_bplus_key_set_unknown (struct adftool_bplus_key *key,
-				      void *key_value);
+  extern LIBADFTOOL_DLL_EXPORTED
+    void adftool_bplus_key_set_unknown (struct adftool_bplus_key *key,
+					void *key_value);
 
   struct adftool_bplus_parameters;
 
-  struct adftool_bplus_parameters *adftool_bplus_parameters_alloc (void);
+  extern LIBADFTOOL_DLL_EXPORTED
+    struct adftool_bplus_parameters *adftool_bplus_parameters_alloc (void);
 
-  void adftool_bplus_parameters_free (struct adftool_bplus_parameters
-				      *parameters);
+  extern LIBADFTOOL_DLL_EXPORTED
+    void adftool_bplus_parameters_free (struct adftool_bplus_parameters
+					*parameters);
 
-  void adftool_bplus_parameters_set_fetch (struct adftool_bplus_parameters
-					   *parameters,
-					   int (*fetch) (uint32_t, size_t *,
-							 size_t, size_t,
-							 uint32_t *, void *),
-					   void *context);
-
-  void adftool_bplus_parameters_set_compare (struct adftool_bplus_parameters
+  extern LIBADFTOOL_DLL_EXPORTED
+    void adftool_bplus_parameters_set_fetch (struct adftool_bplus_parameters
 					     *parameters,
-					     int (*compare) (const struct
-							     adftool_bplus_key
-							     *,
-							     const struct
-							     adftool_bplus_key
-							     *, int *,
-							     void *),
+					     int (*fetch) (uint32_t, size_t *,
+							   size_t, size_t,
+							   uint32_t *,
+							   void *),
 					     void *context);
 
-  void adftool_bplus_parameters_set_fetch_from_hdf5 (struct
-						     adftool_bplus_parameters
-						     *parameters,
-						     hid_t dataset);
+  extern LIBADFTOOL_DLL_EXPORTED
+    void adftool_bplus_parameters_set_compare (struct adftool_bplus_parameters
+					       *parameters,
+					       int (*compare) (const struct
+							       adftool_bplus_key
+							       *,
+							       const struct
+							       adftool_bplus_key
+							       *, int *,
+							       void *),
+					       void *context);
 
-/* The lookup function. The parameters "fetch" and "compare" must be
-   set. */
+  extern LIBADFTOOL_DLL_EXPORTED
+    void adftool_bplus_parameters_set_fetch_from_hdf5 (struct
+						       adftool_bplus_parameters
+						       *parameters,
+						       hid_t dataset);
 
-  int
+  /* The lookup function. The parameters "fetch" and "compare" must be
+     set. */
+
+  extern LIBADFTOOL_DLL_EXPORTED
+    int
     adftool_bplus_lookup (const struct adftool_bplus_key *needle,
 			  struct adftool_bplus_parameters
 			  *parameters, size_t start, size_t max,
---
+++ src/libadftool/emscripten.cpp
@@ -0,0 +1,29 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
+#include <adftool.h>
+
+#ifdef HAVE_EMSCRIPTEN_BIND_H
+
+#include <emscripten/bind.h>
+
+EMSCRIPTEN_BINDINGS (adftool)
+{
+  function ("with_hdf5", &adftool_with_hdf5);
+  function ("bplus_key_alloc", &adftool_bplus_key_alloc);
+  function ("bplus_key_free", &adftool_bplus_key_free);
+  function ("bplus_key_get_known", &adftool_bplus_key_get_known);
+  function ("bplus_key_get_unknown", &adftool_bplus_key_get_unknown);
+  function ("bplus_key_set_known", &adftool_bplus_key_set_known);
+  function ("bplus_key_set_unknown", &adftool_bplus_key_set_unknown);
+  function ("bplus_parameters_alloc", &adftool_bplus_parameters_alloc);
+  function ("bplus_parameters_free", &adftool_bplus_parameters_free);
+  function ("bplus_parameters_set_fetch",
+	    &adftool_bplus_parameters_set_fetch);
+  function ("bplus_parameters_set_compare",
+	    &adftool_bplus_parameters_set_compare);
+  function ("bplus_lookup", &adftool_bplus_lookup);
+}
+
+#endif /* HAVE_EMSCRIPTEN_BIND_H */
