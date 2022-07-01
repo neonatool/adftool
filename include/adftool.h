cff9db64adc3c5b70cf64e052c6f9125b5a0fee8
Vivien Kraus <vivien.kraus@univ-reims.fr>
2022-07-01 16:24:28 +0200

    Enforce link-time symbol visibility even with emscripten.
    I need to have 2 orthogonal annotations for exported public library
    functions:
    
    - whether emscripten should keep a binding to it;
    - whether the linker exports the symbol in the file.

--- include/adftool.h
+++ include/adftool.h
@@ -7,16 +7,30 @@
 
 #if BUILDING_LIBADFTOOL && HAVE_EMSCRIPTEN_H
 #include "emscripten.h"
-#define LIBADFTOOL_DLL_EXPORTED EMSCRIPTEN_KEEPALIVE
-#elif BUILDING_LIBADFTOOL && HAVE_VISIBILITY
+#define LIBADFTOOL_EMSCRIPTEN_KEEPALIVE EMSCRIPTEN_KEEPALIVE
+#else
+#define LIBADFTOOL_EMSCRIPTEN_KEEPALIVE
+#endif
+
+#if defined _MSC_VER || defined _WIN32 || defined __MINGW32__
+#define LIBADFTOOL_DLL_MADNESS 1
+#else
+#define LIBADFTOOL_DLL_MADNESS 0
+#endif
+
+#if BUILDING_LIBADFTOOL && HAVE_VISIBILITY
 #define LIBADFTOOL_DLL_EXPORTED __attribute__((__visibility__("default")))
-#elif BUILDING_LIBADFTOOL && defined _MSC_VER
+#elif BUILDING_LIBADFTOOL && LIBADFTOOL_DLL_MADNESS
 #define LIBADFTOOL_DLL_EXPORTED __declspec(dllexport)
-#elif defined _MSC_VER
+#elif LIBADFTOOL_DLL_MADNESS
 #define LIBADFTOOL_DLL_EXPORTED __declspec(dllimport)
 #else
 #define LIBADFTOOL_DLL_EXPORTED
 #endif
+
+#define LIBADFTOOL_API \
+  LIBADFTOOL_EMSCRIPTEN_KEEPALIVE \
+  LIBADFTOOL_DLL_EXPORTED
 
 #ifdef __cplusplus
 extern "C"
@@ -31,40 +45,40 @@ extern "C"
 
   struct adftool_bplus_key;
 
-  extern LIBADFTOOL_DLL_EXPORTED
+  extern LIBADFTOOL_API
     struct adftool_bplus_key *adftool_bplus_key_alloc (void);
 
-  extern LIBADFTOOL_DLL_EXPORTED
+  extern LIBADFTOOL_API
     void adftool_bplus_key_free (struct adftool_bplus_key *key);
 
   /* Return 0 if no error, something else if the key is not of the
      correct type. */
-  extern LIBADFTOOL_DLL_EXPORTED
+  extern LIBADFTOOL_API
     int adftool_bplus_key_get_known (const struct adftool_bplus_key *key,
 				     uint32_t * index);
 
-  extern LIBADFTOOL_DLL_EXPORTED
+  extern LIBADFTOOL_API
     int adftool_bplus_key_get_unknown (const struct adftool_bplus_key *key,
 				       void **key_value);
 
-  extern LIBADFTOOL_DLL_EXPORTED
+  extern LIBADFTOOL_API
     void adftool_bplus_key_set_known (struct adftool_bplus_key *key,
 				      uint32_t key_index);
 
-  extern LIBADFTOOL_DLL_EXPORTED
+  extern LIBADFTOOL_API
     void adftool_bplus_key_set_unknown (struct adftool_bplus_key *key,
 					void *key_value);
 
   struct adftool_bplus_parameters;
 
-  extern LIBADFTOOL_DLL_EXPORTED
+  extern LIBADFTOOL_API
     struct adftool_bplus_parameters *adftool_bplus_parameters_alloc (void);
 
-  extern LIBADFTOOL_DLL_EXPORTED
+  extern LIBADFTOOL_API
     void adftool_bplus_parameters_free (struct adftool_bplus_parameters
 					*parameters);
 
-  extern LIBADFTOOL_DLL_EXPORTED
+  extern LIBADFTOOL_API
     void adftool_bplus_parameters_set_fetch (struct adftool_bplus_parameters
 					     *parameters,
 					     int (*fetch) (uint32_t, size_t *,
@@ -73,7 +87,7 @@ extern "C"
 							   void *),
 					     void *context);
 
-  extern LIBADFTOOL_DLL_EXPORTED
+  extern LIBADFTOOL_API
     void adftool_bplus_parameters_set_compare (struct adftool_bplus_parameters
 					       *parameters,
 					       int (*compare) (const struct
@@ -85,7 +99,7 @@ extern "C"
 							       void *),
 					       void *context);
 
-  extern LIBADFTOOL_DLL_EXPORTED
+  extern LIBADFTOOL_API
     void adftool_bplus_parameters_set_fetch_from_hdf5 (struct
 						       adftool_bplus_parameters
 						       *parameters,
@@ -94,7 +108,7 @@ extern "C"
   /* The lookup function. The parameters "fetch" and "compare" must be
      set. */
 
-  extern LIBADFTOOL_DLL_EXPORTED
+  extern LIBADFTOOL_API
     int
     adftool_bplus_lookup (const struct adftool_bplus_key *needle,
 			  struct adftool_bplus_parameters
