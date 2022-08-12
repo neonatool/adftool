458d340d8c5ebf9399b4154e8e875226a7f7c81d
Vivien Kraus <vivien.kraus@univ-reims.fr>
2022-08-13 00:04:40 +0200

    Update the exported API for emscripten.

--- src/libadftool/emscripten.cpp
+++ src/libadftool/emscripten.cpp
@@ -8,20 +8,57 @@
 
 #include <emscripten/bind.h>
 
+#define EXPORT(f) function (#f, &f)
+
 EMSCRIPTEN_BINDINGS (adftool)
 {
-  function ("with_hdf5", &adftool_with_hdf5);
-  function ("bplus_key_alloc", &adftool_bplus_key_alloc);
-  function ("bplus_key_free", &adftool_bplus_key_free);
-  function ("bplus_key_get_known", &adftool_bplus_key_get_known);
-  function ("bplus_key_get_unknown", &adftool_bplus_key_get_unknown);
-  function ("bplus_key_set_known", &adftool_bplus_key_set_known);
-  function ("bplus_key_set_unknown", &adftool_bplus_key_set_unknown);
-  function ("bplus_alloc", &adftool_bplus_alloc);
-  function ("bplus_free", &adftool_bplus_free);
-  function ("bplus_set_fetch", &adftool_bplus_set_fetch);
-  function ("bplus_set_compare", &adftool_bplus_set_compare);
-  function ("bplus_lookup", &adftool_bplus_lookup);
+  EXPORT (adftool_file_alloc);
+  EXPORT (adftool_file_free);
+  EXPORT (adftool_file_open);
+  EXPORT (adftool_file_close);
+  EXPORT (adftool_term_alloc);
+  EXPORT (adftool_term_free);
+  EXPORT (adftool_term_copy);
+  EXPORT (adftool_term_set_blank);
+  EXPORT (adftool_term_set_named);
+  EXPORT (adftool_term_set_literal);
+  EXPORT (adftool_term_is_blank);
+  EXPORT (adftool_term_is_named);
+  EXPORT (adftool_term_is_literal);
+  EXPORT (adftool_term_is_typed_literal);
+  EXPORT (adftool_term_is_langstring);
+  EXPORT (adftool_term_value);
+  EXPORT (adftool_term_meta);
+  EXPORT (adftool_term_compare);
+  EXPORT (adftool_term_decode);
+  EXPORT (adftool_term_encode);
+  EXPORT (adftool_term_parse_n3);
+  EXPORT (adftool_statement_alloc);
+  EXPORT (adftool_statement_free);
+  EXPORT (adftool_statement_set_subject);
+  EXPORT (adftool_statement_set_predicate);
+  EXPORT (adftool_statement_set_object);
+  EXPORT (adftool_statement_set_graph);
+  EXPORT (adftool_statement_set_deletion_date);
+  EXPORT (adftool_statement_get_subject);
+  EXPORT (adftool_statement_get_predicate);
+  EXPORT (adftool_statement_get_object);
+  EXPORT (adftool_statement_get_graph);
+  EXPORT (adftool_statement_get_deletion_date);
+  EXPORT (adftool_statement_copy);
+  EXPORT (adftool_statement_compare);
+  EXPORT (adftool_quads_get);
+  EXPORT (adftool_quads_delete);
+  EXPORT (adftool_quads_insert);
+  EXPORT (adftool_results_alloc);
+  EXPORT (adftool_results_free);
+  EXPORT (adftool_results_count);
+  EXPORT (adftool_results_get);
+  EXPORT (adftool_results_resize);
+  EXPORT (adftool_results_set);
+  EXPORT (adftool_lookup);
+  EXPORT (adftool_delete);
+  EXPORT (adftool_insert);
 }
 
 #endif /* HAVE_EMSCRIPTEN_BIND_H */
