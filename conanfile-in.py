fa27c56d0a6d1efdeb8ef17f105a4867470dd550
Vivien Kraus <vivien.kraus@univ-reims.fr>
2022-09-09 16:41:12 +0200

    Set the python path when installing adftool with conan.
    I still have to figure out how to replace "3.10" with the cpython
    version conan uses, but for now it’s OK.

--- conanfile-in.py
+++ conanfile-in.py
@@ -4,7 +4,9 @@ from conan.tools.gnu import Autotools
 class Adftool(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     name = "adftool"
-    requires = "gettext/[>0.0.0]", "hdf5/[>0.0.0]", "gmp/[>0.0.0]", "cpython/[>0.0.0]", "zlib/[>0.0.0]"
+    # FIXME: Don’t hardcode the python version (also, change the
+    # PYTHONPATH after installation, see later)
+    requires = "gettext/[>0.0.0]", "hdf5/[>0.0.0]", "gmp/[>0.0.0]", "cpython/3.10.0", "zlib/[>0.0.0]"
     version = "@VERSION@"
     generators = "AutotoolsToolchain", "AutotoolsDeps"
     exports_sources = "*"
@@ -14,3 +16,7 @@ class Adftool(ConanFile):
         autotools.configure(args=["--enable-relocatable"])
         autotools.make()
         autotools.install()
+
+    def package_info(self):
+        # FIXME: Don’t hardcode 3.10 here.
+        self.env_info.PYTHONPATH.append(self.package_folder + '/lib/python3.10/site-packages')
