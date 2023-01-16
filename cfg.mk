1c402879a5325a6db7ccbf9e584704bdf4da4230
Vivien Kraus <vivien.kraus@univ-reims.fr>
2023-01-16 11:05:32 +0100

    maint: post-release administrivia
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * cfg.mk (old_NEWS_hash): Auto-update.

--- cfg.mk
+++ cfg.mk
@@ -1,6 +1,6 @@
 VC_LIST_ALWAYS_EXCLUDE_REGEX = \
   ^H5make_libsettings_true_main.c$$
 
-old_NEWS_hash = 0ad3daed4899741887c8b71c9f2e0cb0
+old_NEWS_hash = b91f1a6dba9da3e303dda6df1a4874e3
 
 local-checks-to-skip = sc_const_long_option
